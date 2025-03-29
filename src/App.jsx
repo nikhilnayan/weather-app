3d245a42f569649b5873c43120012ba34dbe9ee0
Nikhil Nayan <nayannikhil25@gmail.com>
2025-03-29 17:51:06 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+// import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className='text-red-500'> hi </div>
+    </>
+  )
+}
+
+export default App
